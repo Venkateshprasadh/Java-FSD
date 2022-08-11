36510441609e560598ff60b22c69136d9a124752
Venkateshprasadh <93995646+Venkateshprasadh@users.noreply.github.com>
2022-08-11 10:13:01 +0530

    Done

---
+++ Phase2 AssistedProjects Table-1/src/main/filterdemo/src/AgeFilter.java
@@ -0,0 +1,61 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+/**
+ * Servlet Filter implementation class AgeFilter
+ */
+public class AgeFilter implements Filter {
+
+    /**
+     * Default constructor. 
+     */
+    public AgeFilter() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		// TODO Auto-generated method stub
+		// place your code here
+		int a =Integer.parseInt(request.getParameter("age"));
+		PrintWriter pw = response.getWriter();
+		if(a>=18)
+		{
+			chain.doFilter(request, response);
+			
+		}
+		else
+		{
+			pw.println("Sorry! you are not eligible for voting "+request.getParameter("user"));
+		}
+
+		// pass the request along the filter chain
+		
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+}
