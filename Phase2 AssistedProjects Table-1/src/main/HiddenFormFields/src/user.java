f37f14ca76d640700dfc405a3009ef3998fc9a88
Venkateshprasadh <93995646+Venkateshprasadh@users.noreply.github.com>
2022-08-11 16:14:00 +0530

    Done

---
+++ Phase2 AssistedProjects Table-1/src/main/HiddenFormFields/src/user.java
@@ -0,0 +1,52 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class user
+ */
+public class user extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public user() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String user=request.getParameter("user");
+		PrintWriter pw=response.getWriter();
+		response.setContentType("text/html");
+		pw.print(
+				"<html>"
+				+ "<body>"
+				+ "<form action='userprofile'>"
+				+ "<input type='password' name='password'>"
+				+ "<input type='hidden' name='user' value="+"'"+user+"'>"				
+				+ "<input type='submit' value='login'>"
+				+ "</body>"
+				+ "</html>"
+				);
+	}
+
+}
