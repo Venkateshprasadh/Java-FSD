fd4b527438d1423c196e3cdd1f0265ae60ea4493
Venkateshprasadh <93995646+Venkateshprasadh@users.noreply.github.com>
2022-08-11 15:52:59 +0530

    Session management and Login logout

---
+++ Phase2 AssistedProjects Table-1/src/main/session management/src/profile.java
@@ -0,0 +1,57 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.security.auth.message.callback.PrivateKeyCallback.Request;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class profile
+ */
+public class profile extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public profile() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		PrintWriter pw = response.getWriter();
+		RequestDispatcher rd = request.getRequestDispatcher("index.html");
+		rd.include(request, response);
+		HttpSession session = request.getSession();
+		String name = (String)session.getAttribute("username");
+		String password=(String)session.getAttribute("password");		
+		if(name!=null)
+		{
+			pw.println("Welcome "+name);
+		}
+		else
+		{
+			pw.println("Sorry! you have logged out your session has ended Login again");
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
